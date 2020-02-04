469d43815178c016c1b186908076a2c6d79e1bdf
tmxdyf@163.com <yong.chen3@itel-mobile.com>
2020-02-04 15:04:17 +0800

    Migrate to AndroidX

--- cropper/src/com/edmodo/cropper/util/HandleUtil.java
+++ cropper/src/com/edmodo/cropper/util/HandleUtil.java
@@ -14,7 +14,9 @@
 package com.edmodo.cropper.util;
 
 import android.graphics.PointF;
-import android.support.annotation.NonNull;
+
+
+import androidx.annotation.NonNull;
 
 import com.edmodo.cropper.cropwindow.handle.Handle;
 
